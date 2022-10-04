442cd03344e1d7ec20c42ea64bdc135e34a1e146
MegaMagicarp <66321765+MegaMagicarp@users.noreply.github.com>
2022-10-04 18:17:33 -0400

    Update Shield.cs

--- Assets/__Scripts/Shield.cs
+++ Assets/__Scripts/Shield.cs
@@ -1,25 +1,67 @@
 ﻿using System.Collections;
+
 using System.Collections.Generic;
+
 using UnityEngine;
 
-public class Shield : MonoBehaviour {
+
+
+public class Shield : MonoBehaviour
+{
+
     [Header("Set in Inspector")]
-    bool placeholder1; // here to keep VS from freaking out - DELETE IT
+
+    public float rotationsPerSecond = 0.1f;
+
+
 
     [Header("Set Dynamically")]
-    bool placeholder2; // here to keep VS from freaking out - DELETE IT
+
+    public int levelShown = 0;
 
 
 
-    // Use this for initialization
-    void Start()
+    // This non-public variable will not appear in the Inspector
+
+    Material mat;                                                   // a
+
+
+
+    void Start()
     {
-       
-    }
-	
-	// Update is called once per frame
-	void Update()
+
+        mat = GetComponent<Renderer>().material;                           // b
+
+    }
+
+
+
+    void Update()
     {
-       
+
+        // Read the current shield level from the Hero Singleton
+
+        int currLevel = Mathf.FloorToInt(Hero.S.shieldLevel);            // c
+
+        // If this is different from levelShown...
+
+        if (levelShown != currLevel)
+        {
+
+            levelShown = currLevel;
+
+            // Adjust the texture offset to show different shield level
+
+            mat.mainTextureOffset = new Vector2(0.2f * levelShown, 0);     // d
+
+        }
+
+        // Rotate the shield a bit every frame in a time-based way
+
+        float rZ = -(rotationsPerSecond * Time.time * 360) % 360f;             // e
+
+        transform.rotation = Quaternion.Euler(0, 0, rZ);
+
     }
+
 }
