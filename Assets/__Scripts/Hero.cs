b54ad8756321ba7853c7b12ee9bd98af49d7fb5f
MegaMagicarp <66321765+MegaMagicarp@users.noreply.github.com>
2022-10-04 18:16:27 -0400

    added the hero script

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -2,43 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hero : MonoBehaviour {
-    static public Hero S; // Singleton
+public class Hero : MonoBehaviour
+{
+    static public Hero S; // Singleton                               // a
 
-    [Header("Set in Inspector")]
-    // These fields control the movement of the ship
+
+
+    [Header("Set in Inspector")]
+    // These fields control the movement of the ship
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
 
     [Header("Set Dynamically")]
+    public float shieldLevel = 1;
 
-
-
-
-
-
-	void Start()
+    void Awake()
     {
-
-    }
-	
-	// Update is called once per frame
-	void Update()
-    {
-    
+        if (S == null)
+        {
+            S = this; // Set the Singleton                                   // a
+        }
+        else
+        {
+            Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
+        }
     }
 
-    private void OnTriggerEnter(Collider other)
+    void Update()
     {
+        // Pull in information from the Input class
+        float xAxis = Input.GetAxis("Horizontal");                            // b
+        float yAxis = Input.GetAxis("Vertical");                              // b
 
+        // Change transform.position based on the axes
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        // Rotate the ship to make it feel more dynamic                      // c
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
     }
-
-    public void AbsorbPowerUp(GameObject go)
-    {
-
-    }
-
-    public float shieldLevel
-    {
-        get;set;
-    }
-
 }
