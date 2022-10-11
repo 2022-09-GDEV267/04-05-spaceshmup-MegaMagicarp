097fbf4da0048fa936a834a1e27acd5a07ff37d7
MegaMagicarp <66321765+MegaMagicarp@users.noreply.github.com>
2022-10-11 15:24:51 -0400

    imported package

--- Assets/__Scripts/Enemy.cs
+++ Assets/__Scripts/Enemy.cs
@@ -1,65 +1,134 @@
-﻿using System.Collections;          // Required for Arrays & other Collections
-using System.Collections.Generic;  // Required for Lists and Dictionaries
-using UnityEngine;                 // Required for Unity
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class Enemy : MonoBehaviour
+
+namespace ShmupPlus
 {
-    [Header("Set in Inspector: Enemy")]
-    public float speed = 10f;      // The speed in m/s
-    public float fireRate = 0.3f;  // Seconds/shot (Unused)
-    public float health = 10;
-    public int score = 100;      // Points earned for destroying this
 
-    private BoundsCheck bndCheck;                                            // a
-
-    void Awake()
+    public class Enemy : MonoBehaviour
     {
-        bndCheck = GetComponent<BoundsCheck>();
-    }
 
-    // This is a Property: A method that acts like a field
-    public Vector3 pos
-    {
-        get
+        [Header("Set in Inspector: Enemy")]
+        public float speed = 10f; // The speed in m/s
+        public float fireRate = 0.3f; // Seconds/shot (Unused)
+        public float health = 10;
+        public int score = 100; // Points earned for destroying this
+        public float showDamageDuration = 0.1f; // # seconds to show damage
+        public float powerUpDropChance = 1f; // Chance to drop a power-up
+
+        [Header("Set Dynamically: Enemy")]
+        public Color[] originalColors;
+        public Material[] materials;// All the Materials of this & its children
+        public bool showingDamage = false;
+        public float damageDoneTime; // Time to stop showing damage
+        public bool notifiedOfDestruction = false; // Will be used later
+
+        protected BoundsCheck bndCheck;
+
+        private void Awake()
         {
-            return (this.transform.position);
+            bndCheck = GetComponent<BoundsCheck>();
+            // Get materials and colors for this GameObject and its children
+            materials = Utils.GetAllMaterials(gameObject);
+            originalColors = new Color[materials.Length];
+            for (int i = 0; i < materials.Length; i++)
+            {
+                originalColors[i] = materials[i].color;
+            }
         }
 
-        set
+        // This is a property: A method that acts like a field
+        public Vector3 pos
         {
-            this.transform.position = value;
+            get
+            {
+                return (this.transform.position);
+            }
+            set
+            {
+                this.transform.position = value;
+            }
         }
-    }
 
-    void Update()
-    {
-        Move();
-        if (bndCheck != null && bndCheck.offDown)
+        void Update()
         {
-            // We're off the bottom, so destroy this GameObject
-            Destroy(gameObject);
+            Move();
+
+            if (showingDamage && Time.time > damageDoneTime)
+            {
+                UnShowDamage();
+            }
+
+            if (bndCheck != null && bndCheck.offDown)
+            {
+                // We're off the bottom, so destroy this GameObject
+                Destroy(gameObject);
+            }
         }
-    }
 
-    public virtual void Move()
-    {
-        Vector3 tempPos = pos;
-        tempPos.y -= speed * Time.deltaTime;
-        pos = tempPos;
-    }
-
-    void OnCollisionEnter(Collision coll)
-    {
-        GameObject otherGO = coll.gameObject;
-
-        if (otherGO.tag == "ProjectileHero")
+        public virtual void Move()
         {
-            Destroy(otherGO);        // Destroy the Projectile
-            Destroy(gameObject);     // Destroy this Enemy GameObject
-        }
-        else
+            Vector3 tempPos = pos;
+            tempPos.y -= speed * Time.deltaTime;
+            pos = tempPos;
+        }
+
+        private void OnCollisionEnter(Collision coll)
         {
-            print("Enemy hit by non-ProjectileHero: " + otherGO.name);     // c
-        }
+            GameObject otherGO = coll.gameObject;
+            switch (otherGO.tag)
+            {
+                case "ProjectileHero":
+                    Projectile p = otherGO.GetComponent<Projectile>();
+                    // If this Enemy is off screen, don't damage it.
+                    if (!bndCheck.isOnScreen)
+                    {
+                        Destroy(otherGO);
+                        break;
+                    }
+
+                    // Hurt this Enemy
+                    ShowDamage();
+                    // Get the damage amount from the Main WEAP_DICT
+                    health -= Main.GetWeaponDefinition(p.type).damageOnHit;
+                    if (health <= 0)
+                    {
+                        // Tell the Main singleton that this ship was destroyed
+                        if (!notifiedOfDestruction)
+                        {
+                            Main.S.ShipDestroyed(this);
+                        }
+                        notifiedOfDestruction = true;
+                        // Destroy this enemy
+                        Destroy(this.gameObject);
+                    }
+                    Destroy(otherGO);
+                    break;
+
+                default:
+                    print("Enemy hit by non-ProjectileHero: " + otherGO.name);
+                    break;
+            }
+        }
+
+        void ShowDamage()
+        {
+            foreach (Material m in materials)
+            {
+                m.color = Color.red;
+            }
+            showingDamage = true;
+            damageDoneTime = Time.time + showDamageDuration;
+        }
+
+        void UnShowDamage()
+        {
+            for (int i = 0; i < materials.Length; i++)
+            {
+                materials[i].color = originalColors[i];
+            }
+            showingDamage = false;
+        }
     }
 }
