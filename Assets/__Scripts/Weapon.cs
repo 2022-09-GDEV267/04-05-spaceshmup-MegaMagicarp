ba7954f6611e60536c7091e46487f3db8f005a5b
MegaMagicarp <66321765+MegaMagicarp@users.noreply.github.com>
2022-10-11 18:57:10 -0400

    worked on all the enemy scripts and worked on the weapon script

---
+++ Assets/__Scripts/Weapon.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This is an enum of the various possible weapon types.
+/// It also includes a "shield" type to allow a shield power-up.
+/// Items marked [NI] below are Not Implemented in the IGDPD book.
+/// </summary>
+
+public enum WeaponType
+{
+    none,       // The default / no weapon
+    blaster,    // A simple blaster
+    spread,     // Two shots simultaneously
+    phaser,     // [NI] Shots that move in waves
+    missile,    // [NI] Homing missiles
+    laser,      // [NI]Damage over time
+    shield      // Raise shieldLevel
+}
+
+public class Weapon : MonoBehaviour
+{
+
+}
